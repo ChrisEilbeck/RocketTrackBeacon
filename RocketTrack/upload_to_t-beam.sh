95261fb4cdaed42d280b1a2cdafda52551784405
Chris Eilbeck <chris@hyperspace.org.uk>
2025-05-25 23:59:30 +0100

    Adopted upload script from RocketTrackReceiver

--- RocketTrack/upload_to_t-beam.sh
+++ RocketTrack/upload_to_t-beam.sh
@@ -10,10 +10,15 @@ rm data/*~ 2>/dev/null
 
 mkspiffs -c data -b 4096 -p 256 -s 0x160000 rockettrack.spiffs.bin
 
-python3 `find ~/.arduino15/packages/esp32/tools/esptool_py/ -name esptool.py -print` --chip esp32 \
-    --port ${PORT} \
-    --baud 921600 \
-    write_flash -z 0x290000 rockettrack.spiffs.bin
+`find ~/.arduino15/packages/esp32/tools/esptool_py/ -name esptool -print` --chip esp32 \
+	--port ${PORT} \
+	--baud 921600 \
+	write_flash -z 0x290000 rockettrack.spiffs.bin
+
+#python3 `find ~/.arduino15/packages/esp32/tools/esptool_py/ -name esptool.py -print` --chip esp32 \
+#    --port ${PORT} \
+#    --baud 921600 \
+#    write_flash -z 0x290000 rockettrack.spiffs.bin
 
 #esptool \
 #	--chip esp32 \
